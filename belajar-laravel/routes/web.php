0066570873d01b75394b2b742fa2c6d7876e6a05
yaya <adilah24ti@mahasiswa.pcr.ac.id>
2025-10-16 07:54:07 +0700

    Ini perubahan kedua P6

--- belajar-laravel/routes/web.php
+++ belajar-laravel/routes/web.php
@@ -12,6 +12,8 @@ use App\Http\Controllers\PegawaiController;
 
 use App\Http\Controllers\DashboardController;
 
+use App\Http\Controllers\PelangganController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -50,3 +52,5 @@ Route::post('question/store', [QuestionController::class, 'store'])
 
 
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
