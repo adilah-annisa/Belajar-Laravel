cbfabeda712184d975a60ffba6542013a55830ae
adilah-annisa <adilah24ti@mahasiswa.pcr.ac.id>
2025-12-04 06:25:18 +0700

    p12

---
+++ laragon-6.0-minimal/www/belajar-laravel/app/Models/Multipleuploads.php
@@ -0,0 +1,14 @@
+// Laravel model Multipleuploads
+
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Multipleuploads extends Model
+{
+    protected $table ='multiuploads';
+    protected $primaryKey = 'id';
+    protected $fillable = array('filename','created_at','updated_at');
+}
